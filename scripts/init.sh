cfcc58f0e4bc31b30d8220be3e676fea00812367
Kay Spann <87828715+KayDVC@users.noreply.github.com>
2023-03-24 20:57:04 -0400

    Base Dev Environment (#1)
    * updated dockerfile
    
    * added dev container
    
    * updated dev containr
    
    * updated environment
    
    * removed d2

---
+++ scripts/init.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# A collection of container initialization tasks. Includes 
+# permission changes and installing dependencies.
+
+task_list='grant_ownership prep_scripts install_dev_dependencies'
+tasks=$(echo "$task_list" | wc -w)
+completed=0
+
+echo "---------------------------------"
+echo "Logged in as: $(whoami)"
+echo "Running startup script; Remaining Tasks: ${tasks}"
+
+grant_ownership() {
+    echo "Changing directory ownership from root to NRuser"
+    
+    sudo chown -R NRuser /home/NRuser
+
+    display_progress
+}
+
+prep_scripts (){
+
+    echo "Granting execute perms for all scripts in folder."
+
+    for script in ./scripts/*; do echo "Modifying $script " && chmod +x $script; done;
+    
+    display_progress
+}
+
+install_dev_dependencies () {
+    echo "Installing dependencies"
+
+    # if requirements file listing all python modules doesn't exist, create it.
+    if [ ! -e "requirements.txt" ]; then 
+        echo "No requirements document found. Creating: requirements.txt"
+        pip3 freeze > requirements.txt 
+    fi
+    
+    # install all python modules.
+    pip install -r requirements.txt
+
+    display_progress
+}
+
+display_progress() {
+    completed=$((completed+1))
+    echo "Tasks completed => ${completed} / ${tasks}"
+}
+
+# Run all tasks
+for t in $task_list; do $t; done
+
+echo "---------------------------------"
+echo "Startup script completed."
+
