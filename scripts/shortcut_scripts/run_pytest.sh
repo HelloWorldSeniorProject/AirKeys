5164771bf697953bbe036a499719cf98a84cd5d1
kayDVC <kayydv1016@gmail.com>
2023-07-30 21:40:52 +0000

    testing

--- scripts/shortcut_scripts/run_pytest.sh
+++ scripts/shortcut_scripts/run_pytest.sh
@@ -2,6 +2,8 @@
 
 . ${HOME}/.bashrc
 
+echo "This is home $(HOME)"
+
 # Run Pytest from testing folder. Gives users the ability to specify to only run certain files or functions within files.
 filename=""
 funcname=""
