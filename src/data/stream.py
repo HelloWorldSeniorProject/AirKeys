80c88cf72db3eed00dfb53692058dd6e18691960
kylewstew <97928433+kylewstew@users.noreply.github.com>
2023-11-15 15:22:55 -0500

    Update stream.py
    Refactored code into InputStream class for clarity and to make code more modular

--- src/data/stream.py
+++ src/data/stream.py
@@ -2,116 +2,109 @@ import cv2
 import mediapipe as mp
 from time import time
 
-# mediapipe setup
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mphands = mp.solutions.hands
+class InputStream:
+    def __init__(self):
+        # Initialize camera constants
+        self.CAP_BUFFER_SIZE = 2
+        self.CAMERA_WIDTH = 640
+        self.CAMERA_HEIGHT = 480
+        
+        #Initialize MediaPipe tools 
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands()
+        
+        #Tracking for FPS calculation
+        self.old_frame_time = 0
 
-# constants
-cap_buffer_size = 2
-cam_width = 640
-cam_height = 480
+    def setVideoCaptureAttributes(self, cap):
+        # Returns True if camera attributes are successful
+        try:
+            # Sets buffer size and resolution for the camera capture.
+            # Reduces buffer size to 2 which reduces latency and improves  
+            # capture speed due to frames being processed more immediately.
+            cap.set(cv2.CAP_PROP_BUFFERSIZE, self.CAP_BUFFER_SIZE)
+            
+            # Sets capture resolution to low-res.
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.CAMERA_WIDTH)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.CAMERA_HEIGHT)
+            
+            return True
+        except:
+            return False
 
-# fps tracking
-old_frame_t = 0
-new_frame_t = 0
+    def setupCameras(self, numCameras):
+        # Initializes a specified number of cameras(usually 2) along with 
+        # there attributes and returns a list of VideoCapture objects.
+        cameras = []
+        for i in range(numCameras):
+            cap = cv2.VideoCapture(i)
+            if self.setVideoCaptureAttributes(cap):
+                cameras.append(cap)
+            else:
+                print(f"Failed to set up camera {i}")
+        return cameras
 
+    def process_frame(self, frame):
+        # Process a single frame for hand tracking, including recoloring and landmark drawing.
+        # Recolors frame because MediaPipe requires RGB format.
+        frame_rgb = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+        # Detects hand landmarks with MediaPipe. 
+        results = self.hands.process(frame_rgb)
+        # Converts back to BGR for OpenCV.
+        frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
+        # Draws hand landmarks
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    frame_bgr, hand_landmarks, self.mp_hands.HAND_CONNECTIONS
+                )
+        return frame_bgr  
+      
+    def getFrameData(self, captures):
+        # Retrieves each captured frame from each camera 
+        # and returns a list of frame data.
+        frames = []
+        for cap in captures:
+            ret, frame = cap.read()
+            if ret:
+                processed_frame = self.process_frame(frame)
+                frames.append(processed_frame.tolist())  
+        return frames
 
-def main(left, right, hands, old_frame_t, new_frame_t):
-    while True:
-        _, lf = left.read()
-        _, rf = right.read()
+    
+    def calculate_fps(self):
+        # Calculate and return the current frames per second.
+        new_frame_time = time()
+        fps = int(1 / (new_frame_time - self.old_frame_time))
+        self.old_frame_time = new_frame_time
+        return str(fps)
 
-        # Remove mediapipe processing by commenting from here ---->
+    def teardownCameras(self, captures):
+        # Releases initialized cameras and destroys OpenCV windows.
+        for cap in captures:
+            cap.release()
+        cv2.destroyAllWindows()
 
-        # recolor frame; required for mediapipe proc.
-        lf = cv2.cvtColor(cv2.flip(lf, 1), cv2.COLOR_BGR2RGB)
-        rf = cv2.cvtColor(cv2.flip(rf, 1), cv2.COLOR_BGR2RGB)
+    def run(self):
+        cameras = self.setupCameras(2)  # Assumes 2 cameras
+        try:
+            while True:
+                frames = self.getFrameData(cameras)
+                # Goes over each frame.
+                for idx, frame in enumerate(frames):
+                    fps = self.calculate_fps()
+                    
+                    # Adjusts parameters and puts fps over the image.
+                    cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (100, 255, 0), 2, cv2.LINE_AA)
+                    # Displays the frame
+                    cv2.imshow(f"Camera {idx}", frame)
 
-        # detect hands
-        l_res = hands.process(lf)
-        r_res = hands.process(rf)
-
-        # return to og color to handle display
-        lf = cv2.cvtColor(lf, cv2.COLOR_RGB2BGR)
-        rf = cv2.cvtColor(rf, cv2.COLOR_RGB2BGR)
-
-        # add outline to hand
-        if l_res.multi_hand_landmarks and r_res.multi_hand_landmarks:
-            for hand_landmarks in l_res.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(lf, hand_landmarks, mphands.HAND_CONNECTIONS)
-            for hand_landmarks in r_res.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(rf, hand_landmarks, mphands.HAND_CONNECTIONS)
-
-        # to here ------>
-
-        # FPS tracking
-        new_frame_t = time()
-        fps = str(int(1 / (new_frame_t - old_frame_t)))
-        old_frame_t = new_frame_t
-
-        # uncomment for cli testing.
-        # print(f"FPS: {fps}")
-
-        # add fps counter to left frame.
-        cv2.putText(lf, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (100, 255, 0), 2, cv2.LINE_AA)
-
-        # show frames in separate windows. Purely for setup and debugging. We won't need to show any of
-        # images we capture during actual runtime.
-        cv2.imshow("left", lf)
-        cv2.imshow("right", rf)
-
-        # press q to exit.
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-
-def setup():
-    def set_video_cap_attrs(cap):
-        # only keep two frames in buffer. Supposedly improves speed.
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, cap_buffer_size)
-
-        # set capture resolution (low-res).
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
-
-    # opencv setup
-    left = cv2.VideoCapture(0)
-    right = cv2.VideoCapture(1)
-    set_video_cap_attrs(left)
-    set_video_cap_attrs(right)
-
-    # more mediapipe setup
-    hands = mphands.Hands()
-
-    # verify attributes as expected.
-    def test_video_cap_attrs(cap):
-        res = (
-            int(cap.get(cv2.CAP_PROP_BUFFERSIZE)) == cap_buffer_size
-            and int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) == cam_width
-            and int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) == cam_height
-        )
-        return res
-
-    assert (test_video_cap_attrs(left) and test_video_cap_attrs(right)) == True
-
-    return left, right, hands
-
-
-def teardown(left, right):
-    left.release()
-    right.release()
-
-    cv2.destroyAllWindows()
-
-
-def run():
-    left, right, hands = setup()
-
-    main(left, right, hands, old_frame_t, new_frame_t)
-
-    teardown(left, right)
-
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+        finally:
+            self.teardownCameras(cameras)
 
 if __name__ == "__main__":
-    run()
+    input_stream = InputStream()
+    input_stream.run()
