19ecf1a0a88251a2be89eaca0825a059c65525e1
kayDVC <kayydv1016@gmail.com>
2023-11-27 15:37:34 -0500

    saving changes

--- src/main.py
+++ src/main.py
@@ -8,26 +8,53 @@ from common.config.config_table import ConfigTable
 from common.file.file_manager import FileManager
 from common.power.power_interface import PowerInterface
 from common.time.internal_clock import InternalClock
+from data.stream import DataStream
+from common.types import *
 
 # AirKeys Utils
 from util.logger import get_logger
 
+# Standard Modules
+import multiprocessing
+
 logger = get_logger("Main.log")
 
-""" Main program thread. Functions as object Factory & Controller """
-
-# TODO : finish
+""" Main program thread. Functions as object Factory & Controller"""
 
 
 def main():
-    # initialize system resources
+    """Initializes all system resources and executes main program loop.
+    """
+
+    # any failure here is critical. Exit program if so.
     try:
-        clock = InternalClock()
+        
+        clock = InternalClock() 
         ct = ConfigTable()
         fm = FileManager()
+        stream = DataStream()
+        # TODO: add data processing interface
+        # processor = DataProcessor()
+        # TODO add connection manager interface
+        
+        # try to load previous config table and set to calibration mode.
+        ct.write(prev_config=fm.read_configuration_file())
 
-        # try to load previous config table
-        ct.write(fm.read_configuration_file())
+        # TODO: attempt to connect to external device
+
+        # setup input and display layout
+        captures = stream.setup_cameras(num_cameras=2)
+        
+
+        # TODO: calibrate system with data processing module. Abstract to utility function once done.
+        ct.set_mode(mode=Mode.Calibration)
+        stream.display_frame(frame=fm.get_layout_file(f_name=ct.get_layout()))
+        calibration_frames = stream.get_frame_data(captures=captures)
+        # 
+
+
+
+
     except Exception as e:
         exit_with_failure(f"Failed to initialize system resources.\n{e}")
 
@@ -38,6 +65,8 @@ def main():
     # Finish current task
     PowerInterface.power_off()
 
+
+    ct.set_mode(mode=Mode.Calibration)
 
 def exit_with_failure(message: str):
     """Exits the program with failure status.
